d4790d92de5b0cf50465999f51647ab5f60f28e1
theophile591300 <thechtidu59@live.fr>
2024-12-05 13:10:34 +0100

    Premier commit

---
+++ src/data/projectsData.js
@@ -0,0 +1,42 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "Jeu MMORPG - DR",
+    date: "Mai 2024",
+    languages: ["JavaScript, HTML, CSS"],
+    infos:
+      "Création d'un jeu MMORPG en utilisant JavaScript, HTML, CSS, Github, API. Gestion des interactions utilisateur et des actions en temps réel. Le jeu comporte une architecture permettant une expansion future et une gestion dynamique des événements.",
+    img: "./assets/img/projet-2.jpg",
+    link: "https://rpg-donjon-vs-dragon.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Site E-commerce Kaishi",
+    date: "Septembre 2024",
+    languages: ["JavaScript, NodeJs, React, MySQL, API, SASS"],
+    infos:
+      "Projet e-commerce/vitrine de vente de matcha développé avec une stack composée de JavaScript, NodeJs, React, MySQL, API, et SASS. Intégration complète de la gestion des utilisateurs, de l'authentification sécurisée et de la gestion de la base de données",
+    img: "./assets/img/projet-1.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 3,
+    title: "Site Vitrine Cabinet médical",
+    date: "Septembre 2024",
+    languages: ["JavaScript, React, Tailwind"],
+    infos:
+      "Projet d'un site web vitrine pour un cabinet médical dévelopé en utilisant JavaScript, React, Tailwind. Le site répond à un besoin de modernisation du précédent site web du cabinet.",
+    img: "./assets/img/projet-3.jpg",
+    link: "https://medical-website-plum.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Flappy bird",
+    date: "Juillet 2020",
+    languages: ["JavasScript,HTML,Css"],
+    infos:
+      "Petit projet de jeu reconstituant un jeu iconique sur mobile.",
+    img: "./assets/img/projet-4.jpg",
+    link: "http://www.google.com",
+  },
+];
---
+++ src/pages/Contact.js
@@ -0,0 +1,84 @@
+import React from "react";
+import ContactForm from "../components/ContactForm";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import SocialNetwork from "../components/SocialNetwork";
+import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 300,
+    },
+  };
+
+  return (
+    <main>
+      <Mouse />
+      <motion.div
+        initial="out"
+        exit="out"
+        animate="in"
+        variants={pageTransition}
+        transition={{ duration: 0.5 }}
+        className="contact"
+      >
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>Adresse</h4>
+              <p>La Timone</p>
+              <p>13005 Marseille</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>Téléphone</h4>
+              <CopyToClipboard text="068147351" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Téléphone copié !")}
+                >
+                  06 81 47 33 51
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>Email</h4>
+              <CopyToClipboard text="theophile.berthe@gmail.com" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Email copié !")}
+                >
+                theophile.berthe@gmail.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>Berthe - 2024</p>
+          </div>
+        </div>
+        <Buttons left={"/projet-4"} />
+      </motion.div>
+    </main>
+  );
+};
+
+export default Contact;
